acff68d488b767b69164e0dc8977facdb9d68369
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-02 02:55:10 +0000

    feat: add Storage module with localStorage save and load

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ class Project {
         this.todos = this.todos.filter(todo => todo.id != id);
     }
 }
+
+const Storage = {
+    saveProjects(projects) {
+        localStorage.setItem("projects", JSON.stringify(projects));
+    },
+
+    loadProjects() {
+        const data = localStorage.getItem("projects");
+        if (!data) return [];
+
+        const parsed = JSON.parse(data);
+
+        return parsed.map(projectData => {
+            const project = new Project(projectData.name);
+            project.id = projectData.id;
+            project.todos = projectData.todos.map(todoData => {
+                return Object.assign(new Todo(), todoData);
+            });
+            return project;
+        });
+    }
+}
