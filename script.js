31e37b3e2610e5268272ec07c629aea8bfc52b37
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-02 02:34:24 +0000

    feat: add Project class with core properties and methods

--- script.js
+++ script.js
@@ -18,9 +18,14 @@ class Project {
     constructor(name) {
         this.id = crypto.randomUUID();
         this.name = name;
+        this.todos = [];
     }
 
     addTodo(todo) {
-        
+        this.todos.push(todo);
+    }
+
+    removeTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id != id);
     }
 }
