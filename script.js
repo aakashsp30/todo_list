2e04d5e7ccfaf67e44479c463f9d08a909dcfc13
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-02 03:40:03 +0000

    feat: add App state module with project and todo management

--- script.js
+++ script.js
@@ -51,3 +51,40 @@ const Storage = {
         });
     }
 }
+
+const App = {
+    projects: [],
+
+    init() {
+        this.projects = Storage.loadProjects();
+        if(this.projects.length == 0) {
+            this.addProject("Default");
+        }
+    },
+
+    addProject(name) {
+        this.projects.push(new Project(name));
+        Storage.saveProjects(this.projects);
+    },
+
+    removeProject(id) {
+        this.projects = this.projects.filter(project => project.id != id);
+        Storage.saveProjects(this.projects);
+    },
+
+    getProject(id) {
+        return this.projects.find(project => project.id === id);
+    },
+
+    addTodo(projectId, todo) {
+        const project = this.getProject(projectId);
+        project.addTodo(todo);
+        Storage.saveProjects(this.projects);
+    },
+
+    removeTodo(projectId, todoId) {
+        const project = this.getProject(projectId);
+        project.removeTodo(todoId);
+        Storage.saveProjects(this.projects);
+    }
+}
