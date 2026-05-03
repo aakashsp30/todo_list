e11b654b709d4011ca9aac9dae08504cef5dd8cd
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-03 05:57:12 +0000

    feat: auto-select first project on load

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import { UI } from "./UI.js";
 App.init();
 UI.init();
 UI.renderProjects();
+
+if(App.projects.length > 0) {
+    UI.currentProjectId = App.projects[0].id;
+    UI.renderTodos(App.projects[0].id);
+}
